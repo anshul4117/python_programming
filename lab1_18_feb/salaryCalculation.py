6ca7d95f9d91c8cd986a2f53e0e93a2d8956ddc0
Anshul <anshul41171@gmail.com>
2026-02-19 10:43:05 +0530

    refactor: clean up salary calculation script by commenting out input lines

--- lab1_18_feb/salaryCalculation.py
+++ lab1_18_feb/salaryCalculation.py
@@ -1,3 +1,11 @@
-baseSalary = int(input("Enter the base salary: "))
-HRA = int(input("Enter the HRA : "))
-HRA = int(input("Enter the Bonous : "))
+basic_salary = 25000
+bonus = 5000
+
+if basic_salary > 0:
+    hra = (20 * basic_salary) / 100
+    total_salary = basic_salary + hra + bonus
+
+    print("HRA:", hra)
+    print("Total Monthly Salary:", total_salary)
+else:
+    print("Invalid basic salary")
