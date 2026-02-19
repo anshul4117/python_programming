5c5ac914c7fb59a0b66da93ab49bd26f9f8619b3
Anshul <anshul41171@gmail.com>
2026-02-19 12:27:30 +0530

    add: implement various scripts including salary calculation, age validation, area and perimeter of circle, angle check for triangle, profit and loss calculation, time conversion, and shopping bill

---
+++ 19_feb/lab/1.whileLoop.py
@@ -0,0 +1,24 @@
+# 1,4,9,16,25,36,----- ,2500 without terms --> pending
+# 1,8,27,64,125,----- ,27000 without terms --> pending
+
+
+
+
+
+# 4,-8,12,-16,20,-24,----- ,-800,804 without terms
+# wrost code ever
+# x=4
+# sign=1
+# while(x<=804):
+#     print(x*sign, end= ",")
+#     sign = sign*-1
+#     x = x + 4
+
+# easy solution
+# x = 4
+# while(x<=804):
+#     if(x%8==0):
+#         print(-x,end=",")
+#     else:
+#         print(x,end=",")
+#     x = x+4
---
+++ 19_feb/lab/2.forLoop.py
@@ -0,0 +1,13 @@
+# simple loop
+# for n in range(10,30,4):
+#     print(n*2,end=",")
+
+# natural numbers
+# for n in range(1,21):
+#     print(n,end=",")
+
+# sum of natural numbers
+Sum = 0
+for n in range(1,21):  
+    Sum += n
+print("sum of first 20 natural numbers : ",Sum)
