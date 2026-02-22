1c261cf429a54a4ed3eb670cb6171d01a63e360a
Anshul <anshul41171@gmail.com>
2026-02-22 22:45:03 +0530

    add: new pattern

--- 20_feb/labWork/1.pattern.py
+++ 20_feb/labWork/1.pattern.py
@@ -41,18 +41,26 @@
 #    *****
 #     ***
 #      *
+# row = int(input("Enter rows : "))
+# for i in range(row,0,-1):
+#     print(" " * i, end="")
+#     print("*" * (row - i + 1 ), end="")
+#     print("*" * (row - i ))
+# for i in range(2, row + 1):
+#     print(" " * i, end="")
+#     print("*" * (row - i + 1 ), end="")
+#     print("*" * (row - i ))
+
+# another star pattern
+# *********
+#  *******
+#   *****
+#    ***
+#     *
+
 row = int(input("Enter rows : "))
 for i in range(row,0,-1):
-    print(" " * i, end="")
-    print("*" * (row - i + 1 ), end="")
-    print("*" * (row - i ))
-for i in range(2, row + 1):
-    print(" " * i, end="")
-    print("*" * (row - i + 1 ), end="")
-    print("*" * (row - i ))
+    print(" " * (row - i), end="")
+    print("*" * (2 * i - 1))  
 
 
-
-
-
-   
