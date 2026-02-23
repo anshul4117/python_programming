5aaf483e1c27d00eab4c9637c2d002d42f49b935
Anshul <anshul41171@gmail.com>
2026-02-23 16:27:39 +0530

    add: clas work using dict and functions

---
+++ 23_feb/claassWork/alphabetCount.py
@@ -0,0 +1,31 @@
+# sentence = input("Enter a sentence: ")
+# count = 0
+# for char in sentence:
+#     if char.isalpha():
+#         count += 1  
+# print("Number of alphabetic characters:", count)
+
+
+# Wap to find out numbers of special characters in a sentence
+# sentence = input("Enter a sentence: ")
+# special_count = 0
+# for char in sentence:
+#     if not char.isalnum():
+#         special_count += 1
+# print("Number of special characters:", special_count)
+
+# isupper case
+# sen = "Anshul"
+# print(sen.isupper())  
+
+# istitle  case
+# sen = "Anshul"
+# print(sen.istitle())  
+
+# upper  case
+# sen = "Anshul"
+# print(sen.upper()) 
+
+# lower  case
+sen = "Anshul"
+print(sen.lower())  
---
+++ 23_feb/claassWork/dictMethod.py
@@ -0,0 +1,11 @@
+sentence = "Hello World"
+print(sentence.isalpha())  
+
+str2 = "hello"
+print(str2.isalpha())  
+
+str3 = "hello 123"
+print(str3.isalpha())  
+
+str4 = "123"
+print(str4.isalnum())  
