505333fbdbabf8c6bcc6c92a682850835d872a6f
Anshul <anshul41171@gmail.com>
2026-02-23 10:11:13 +0530

    add: update the folder structure

---
+++ 20_feb/classPracticeQuetions.py/listOperation.py
@@ -0,0 +1,44 @@
+# creating the 
+number = [12,34,56,78,90,"Anshul","py"]
+
+# display all the elements in the list
+# print("|-----------------------------|")
+# print(number)
+
+# output
+# [12, 34, 56, 78, 90, 'Anshul', 'py']
+
+
+
+# display the all elements in the list one by one
+# for i in number:
+#     print(i)
+# print("|-----------------------------|")
+# output
+# 12
+# 34
+# 56
+# 78
+# 90
+# Anshul
+# py
+
+# # display the elements in reverse order using forward indexing
+# for i in range(len(number)-1, -1, -1):
+#     print(number[i])
+# print("|-----------------------------|")
+# output
+# py
+# Anshul
+# 90
+# 78
+# 56
+# 34
+# 12
+
+# add two list in one list
+# list1 = [1,2,3]
+# list2 = [4,5,6]
+# list3 = list1 + list2
+# print(list3)
+
