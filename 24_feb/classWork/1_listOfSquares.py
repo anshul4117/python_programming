49b9dda660c2b2cb4cc88fb87882d172804bde10
Anshul <anshul41171@gmail.com>
2026-03-01 14:16:36 +0530

    Create a list of squares of numbers from 1 to 10

---
+++ 24_feb/classWork/1_listOfSquares.py
@@ -0,0 +1,11 @@
+# Q1: Create a list of squares of numbers from 1 to 10
+# Output: [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+squares = []
+for i in range(1, 11):
+    squares.append(i * i)
+
+print("Squares from 1 to 10:", squares)
+
+# Output:
+# Squares from 1 to 10: [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
