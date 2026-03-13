460cc42fea139bac65a5933d794b967bc1259190
Anshul <anshul41171@gmail.com>
2026-03-13 12:31:48 +0530

    Numpy an concepts

---
+++ 13_march/classWork/first_Numpy.py
@@ -0,0 +1,51 @@
+# import num py
+import numpy as np
+
+num = np.array([23,32,22])
+# print("Array : " , num)
+# OutPut : Array :  [23 32 22]
+
+# num2 = np.array([21,3.2,20])
+# print("Array2 : " , num2)
+# OutPut : Array2 :  [21.   3.2 20. ]
+
+
+# num3 = np.array([21,3.2,"anshul"])
+# print("Array3 : " , num3)
+# OutPut : Array3 :  ['21' '3.2' 'anshul']
+
+
+# multiply the array with 3
+# print("Array : ", num)
+# print("Array after multiply : ", num * 3)
+# output
+# Array :  [23 32 22]
+# Array after multiply :  [69 96 66]
+
+# greater then 
+# print("Array after num > 30 : ", num > 30)
+# output
+# Array after num > 30 :  [False  True False]
+
+
+
+# double dimension(2D) array
+# mat1 = np.array([[1,3,11,13],[22,3,34,43]]) #same length of both array
+# print("2D array : ", mat1)
+# output
+# [[ 1  3 11 13]
+#  [22  3 34 43]]
+
+
+# Three dimension array
+mat1 = np.array([[[2,3],[1,2]],[[3,4],[5,6]]]) #same length of both array
+print("3D array : \n", mat1)
+# output
+# 3D array : 
+#  [[[2 3]
+#   [1 2]]
+
+#  [[3 4]
+#   [5 6]]
+
+
