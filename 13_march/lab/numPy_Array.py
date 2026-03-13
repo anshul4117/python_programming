c7bcca74cd6dc42db2340fd65cde01f8b1beb030
Anshul <anshul41171@gmail.com>
2026-03-13 12:32:58 +0530

    numpy and some functions

---
+++ 13_march/lab/numPy_Array.py
@@ -0,0 +1,7 @@
+#create a array of 5 elements and import numpy
+
+import numpy as np
+arr = np.array([1,3,1,3,1])
+print("Array : ", arr)
+#multiply each elements by 10
+print("After multiply 10: ", arr * 10)
