6a4f33f178859d0176ce5b469c81e8c147547505
Anshul <anshul41171@gmail.com>
2026-03-31 11:11:35 +0530

    Find mean, median, and standard deviation of an array

---
+++ 31_march/lab/11.meanMedian.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+array = np.array([10, 20, 30, 40, 50, 60, 70])
+
+mean = np.mean(array)
+
+median = np.median(array)
+
+std_dev = np.std(array)
+
+print("Array:", array)
+print("Mean:", mean)
+print("Median:", median)
+print("Standard Deviation:", std_dev)
